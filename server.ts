ef741d01add4e8bf6f5c744d41608df9b533af50
Giuliane Oliveira <giulianeoliveira@MacBook-Pro-de-Giuliane.local>
2025-06-23 13:12:01 -0300

    adding csv parser

--- server.ts
+++ server.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import multer from "multer";
+import fs from "fs";
+import csv from "csv-parser";
 
 const app = express();
 const port = 3000;
@@ -10,12 +12,75 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/upload", upload.single("transaction"), (req, res) => {
+const loadRowsAccordingFileUploaded = (
+  file: Express.Multer.File
+): Promise<Transaction[]> => {
+  return new Promise((resolve, reject) => {
+    const result: Transaction[] = [];
+    const stream = fs
+      .createReadStream(file.path)
+      .pipe(csv())
+      .on("data", async (data) => {
+        result.push(data);
+      })
+      .on("end", () => {
+        resolve(result);
+        fs.unlink(file.path, (err) => {
+          if (err) {
+            console.error("Error deleting file:", err);
+            return;
+          }
+        });
+      })
+      .on("error", (err) => {
+        reject(err);
+      });
+  });
+};
+
+interface Transaction {
+  id: string;
+  from: string;
+  to: string;
+  amount: number;
+}
+
+app.post("/upload", upload.single("transaction"), async (req, res) => {
   const transaction = req.file;
 
   if (!transaction) res.status(400).json("file not sended.");
+  else {
+    const data = await loadRowsAccordingFileUploaded(transaction);
+    // array salva
 
-  res.status(200).json({ transaction });
+    // array invalido
+    // amount negativo
+    // 3 itens iguais
+    // amount acima de 50k
+    //
+
+    const invalidTransactions = data.map((transaction) => {
+      if (transaction.amount < 0) {
+        return {
+          transaction,
+          error: "negative amount.",
+        };
+      }
+
+      // 7733370917404;1176377952903;181940007
+      // transaction.amount / 100
+      const SUSPECT_AMOUNT = 50000;
+
+      if (transaction.amount / 100 < SUSPECT_AMOUNT) {
+        return {
+          transaction,
+          error: "Suspect Amount (>50k).",
+        };
+      }
+    });
+
+    res.status(200).json({ transaction });
+  }
 });
 
 app.listen(port, () => {
