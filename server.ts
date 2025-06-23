763e329d2002e69a5ddf4fae558dff21d77463f0
Giuliane Oliveira <giulianeoliveira@MacBook-Pro-de-Giuliane.local>
2025-06-23 12:14:09 -0300

    integrating multer to upload files

--- server.ts
+++ server.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import multer from "multer";
 
 const app = express();
 const port = 3000;
 
+const upload = multer({ dest: "uploads/" });
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/upload", upload.single("transaction"), (req, res) => {
+  const transaction = req.file;
+
+  if (!transaction) res.status(400).json("file not sended.");
+
+  res.status(200).json({ transaction });
 });
 
 app.listen(port, () => {
